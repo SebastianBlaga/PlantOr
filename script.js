c3a70e25772fc20dbbb954d660b41b7f02a175a3
Carla Petrut <carladiana51@yahoo.com>
2020-05-07 21:29:07 +0300

    log-in-pop-up

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById('login-item').addEventListener('click',
+function(){
+    document.querySelector('.bg-form').style.display='flex';
+});
+
+document.getElementById('buton-cancel').addEventListener('click',
+function(){
+    document.querySelector('.bg-form').style.display='none';
+});
