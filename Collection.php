53031201c8e780fd1132c4eaad1cdae264f56a31
AndreiGrapa <62424180+AndreiGrapa@users.noreply.github.com>
2020-05-30 17:12:04 +0300

    Add files via upload

---
+++ Collection.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="Col1.css">
+        <title>PlantOr</title>
+    </head>
+    <body class="Background">
+         <header>
+            <div class="menu-header">
+                <div class="menu">
+                    <a href="#statistics">Statistics</a>
+                    <a href="#collection">Collection</a>
+                    <a class="active" href="#albums">Albums</a>
+                    <a href="#about">About</a>
+                    <a href="#logout">Log Out</a>
+                </div>
+                <div class="Logo"> <img src='Poze/Logo%20white.svg'></div>
+             </div>
+        </header>
+        <div class="Items">
+    <div class=Bara2>
+    <div class=Bara2Items>
+    <div class=J1>
+       <div class=PlantCol>Your Plant Collection</div>
+       <div class=PlantColText>Here you can see the plants you added to your personal virtual herbarium. Use the Add Plant button to add a new plant to your collection.</div>
+    </div>
+    <div class=J2>
+        <a href="#login">
+        <div class=button1 href="#CollPopup">
+        <div class=buttonText1>Add</div>
+        </div>
+        </a>
+    </div>
+    </div>
+     </div>
+    <div class=Tabel>
+    <div class=J3>
+    <div class=Item1>
+        <div class=C1>
+        <img src="Poze/img_mountains.jpg" alt="Poza" class=p1>
+        </div>
+        <div class=C>
+        <div class=Tname>Technical name</div>
+        <div class=Srn1>Some random name</div>
+        <div class=Tname>Common name</div>
+        <div class=Srn1>Some random name</div>
+        <div class=Tname>Plant group</div>
+        <div class=Srn1>The plant group</div>
+        <div class=Tname>Family</div>
+        <div class=Srn1>The plant family</div>
+        <div class=Tname>Species</div>
+        <div class=Srn1>The plant species</div>
+        <div class=EditButton>
+            <div class=EditTxt>Edit</div>
+            </div>
+        <div class=EditButton>
+            <div class=EditTxt>Remove</div>
+            </div>
+        </div>    
+    </div>
+     <div class=Item1>
+        <div class=C1>
+        <img src="Poze/img_mountains.jpg" alt="Poza" class=p1>
+        </div>
+        <div class=C>
+        <div class=Tname>Technical name</div>
+        <div class=Srn1>Some random name</div>
+        <div class=Tname>Common name</div>
+        <div class=Srn1>Some random name</div>
+        <div class=Tname>Plant group</div>
+        <div class=Srn1>The plant group</div>
+        <div class=Tname>Family</div>
+        <div class=Srn1>The plant family</div>
+        <div class=Tname>Species</div>
+        <div class=Srn1>The plant species</div>
+        <div class=EditButton>
+            <div class=EditTxt>Edit</div>
+            </div>
+        <div class=EditButton>
+            <div class=EditTxt>Remove</div>
+            </div>
+        </div>    
+    </div>
+    </div>
+    <div class=J4>
+    <div class=Item1>
+    <img src="Poze/img_mountains.jpg" alt="Poza" class=p1>
+        <div class=C>
+        <div class=Tname>Technical name</div>
+        <div class=Srn1>Some random name</div>
+        <div class=Tname>Common name</div>
+        <div class=Srn1>Some random name</div>
+        <div class=Tname>Plant group</div>
+        <div class=Srn1>The plant group</div>
+        <div class=Tname>Family</div>
+        <div class=Srn1>The plant family</div>
+        <div class=Tname>Species</div>
+        <div class=Srn1>The plant species</div>
+        <div class=EditButton>
+            <div class=EditTxt>Edit</div>
+            </div>
+        <div class=EditButton>
+            <div class=EditTxt>Remove</div>
+            </div>
+        </div>    
+    </div>
+     <div class=Item1>
+        <div class=C1>
+        <img src="Poze/img_mountains.jpg" alt="Poza" class=p1>
+        </div>
+        <div class=C>
+        <div class=Tname>Technical name</div>
+        <div class=Srn1>Some random name</div>
+        <div class=Tname>Common name</div>
+        <div class=Srn1>Some random name</div>
+        <div class=Tname>Plant group</div>
+        <div class=Srn1>The plant group</div>
+        <div class=Tname>Family</div>
+        <div class=Srn1>The plant family</div>
+        <div class=Tname>Species</div>
+        <div class=Srn1>The plant species</div>
+        <div class=EditButton>
+            <div class=EditTxt>Edit</div>
+            </div>
+        <div class=EditButton>
+            <div class=EditTxt>Remove</div>
+            </div>
+        </div>    
+    </div>
+    </div>       
+    </div>
+        </div>
+</body>
+</html>
---
+++ CollectionPopup.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="Col1.css">
+        <title>PlantOr</title>
+    </head>
+    <div class="Popupbox">
+    <a class="Xbutton" href="Da">  <img src="Poze/x%20button.svg" alt="Poza"></a>
+    <div class="AddPlant">Add Plant</div>
+        
+    <form action="/action_page.php">
+  <label for="fname" class="TechName">Techncal Name*</label><br>
+  <input type="text" id="fname" class="TechName1"><br><br>
+  <label for="fname" class="TechName">Common Name*</label><br>
+  <input type="text" id="fname" class="TechName1"><br><br>
+  <label for="fname" class="TechName">Plant Group*</label><br>
+  <select class="TechName2">
+             <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+        </select>
+  <label for="fname" class="TechName">Family*</label><br>
+  <select class="TechName2">
+             <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+        </select>
+  <label for="fname" class="TechName">Species*</label><br>
+        <select class="TechName2">
+             <option value="volvo">Volvo</option>
+  <option value="saab">Saab</option>
+  <option value="mercedes">Mercedes</option>
+  <option value="audi">Audi</option>
+        </select><br>
+
+  <input type="submit" value="Sign Up" class="Submit1">      
+</form> 
+  <a class="Cancel1" href="Da">Cancel</a>  
+    </div>
+    <body class="Background">
+    <div class="Background1">
+         <header>
+            <div class="menu-header">
+                <div class="menu">
+                    <a href="#statistics">Statistics</a>
+                    <a href="#collection">Collection</a>
+                    <a class="active" href="#albums">Albums</a>
+                    <a href="#about">About</a>
+                    <a href="#logout">Log Out</a>
+                </div>
+                <div class="Logo"> <img src='Poze/Logo%20white.svg'></div>
+             </div>
+        </header>
+        <div class="Items">
+    <div class=Bara2>
+    <div class=Bara2Items>
+    <div class=J1>
+       <div class=PlantCol>Your Plant Collection</div>
+       <div class=PlantColText>Here you can see the plants you added to your personal virtual herbarium. Use the Add Plant button to add a new plant to your collection.</div>
+    </div>
+    <div class=J2>
+        <a href="#login">
+        <div class=button1 href="#AddPlant">
+        <div class=buttonText1>Add</div>
+        </div>
+        </a>
+    </div>
+    </div>
+     </div>
+    <div class=Tabel>
+    <div class=J3>
+    <div class=Item1>
+        <div class=C1>
+        <img src="Poze/img_mountains.jpg" alt="Poza" class=p1>
+        </div>
+        <div class=C>
+        <div class=Tname>Technical name</div>
+        <div class=Srn1>Some random name</div>
+        <div class=Tname>Common name</div>
+        <div class=Srn1>Some random name</div>
+        <div class=Tname>Plant group</div>
+        <div class=Srn1>The plant group</div>
+        <div class=Tname>Family</div>
+        <div class=Srn1>The plant family</div>
+        <div class=Tname>Species</div>
+        <div class=Srn1>The plant species</div>
+        <div class=EditButton>
+            <div class=EditTxt>Edit</div>
+            </div>
+        <div class=EditButton>
+            <div class=EditTxt>Remove</div>
+            </div>
+        </div>    
+    </div>
+     <div class=Item1>
+        <div class=C1>
+        <img src="Poze/img_mountains.jpg" alt="Poza" class=p1>
+        </div>
+        <div class=C>
+        <div class=Tname>Technical name</div>
+        <div class=Srn1>Some random name</div>
+        <div class=Tname>Common name</div>
+        <div class=Srn1>Some random name</div>
+        <div class=Tname>Plant group</div>
+        <div class=Srn1>The plant group</div>
+        <div class=Tname>Family</div>
+        <div class=Srn1>The plant family</div>
+        <div class=Tname>Species</div>
+        <div class=Srn1>The plant species</div>
+        <div class=EditButton>
+            <div class=EditTxt>Edit</div>
+            </div>
+        <div class=EditButton>
+            <div class=EditTxt>Remove</div>
+            </div>
+        </div>    
+    </div>
+    </div>
+    <div class=J4>
+    <div class=Item1>
+    <img src="Poze/img_mountains.jpg" alt="Poza" class=p1>
+        <div class=C>
+        <div class=Tname>Technical name</div>
+        <div class=Srn1>Some random name</div>
+        <div class=Tname>Common name</div>
+        <div class=Srn1>Some random name</div>
+        <div class=Tname>Plant group</div>
+        <div class=Srn1>The plant group</div>
+        <div class=Tname>Family</div>
+        <div class=Srn1>The plant family</div>
+        <div class=Tname>Species</div>
+        <div class=Srn1>The plant species</div>
+        <div class=EditButton>
+            <div class=EditTxt>Edit</div>
+            </div>
+        <div class=EditButton>
+            <div class=EditTxt>Remove</div>
+            </div>
+        </div>    
+    </div>
+     <div class=Item1>
+        <div class=C1>
+        <img src="Poze/img_mountains.jpg" alt="Poza" class=p1>
+        </div>
+        <div class=C>
+        <div class=Tname>Technical name</div>
+        <div class=Srn1>Some random name</div>
+        <div class=Tname>Common name</div>
+        <div class=Srn1>Some random name</div>
+        <div class=Tname>Plant group</div>
+        <div class=Srn1>The plant group</div>
+        <div class=Tname>Family</div>
+        <div class=Srn1>The plant family</div>
+        <div class=Tname>Species</div>
+        <div class=Srn1>The plant species</div>
+        <div class=EditButton>
+            <div class=EditTxt>Edit</div>
+            </div>
+        <div class=EditButton>
+            <div class=EditTxt>Remove</div>
+            </div>
+        </div>    
+    </div>
+    </div>       
+    </div>
+        </div>
+        </div>
+</body>
+</html>
