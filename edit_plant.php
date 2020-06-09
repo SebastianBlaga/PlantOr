5e845094b33514c51250731534c1417494be8367
Carla Petrut <carladiana51@yahoo.com>
2020-06-09 23:05:56 +0300

    edit plant functionality

--- edit_plant.php
+++ edit_plant.php
@@ -2,7 +2,17 @@
 if(isset($_POST['save-submit'])){
 
     require 'db.php';
-    $idpl=$_POST["save-submit"];
-    echo $idpl;
-    echo 'test';
+    $idpl=$_POST['save-submit'];
+    $tn=$_POST['TN'];
+    $cn=$_POST['CN'];
+    $pg=$_POST['PG'];
+    $sp=$_POST['Species'];
+    $fm=$_POST['Family'];
+    $sql="UPDATE plants SET TechnicalName=\"".$tn."\",CommonName=\"".$cn."\",PlantGroup=\"".$pg."\",Family=\"".$fm."\",Species=\"".$sp."\"WHERE idPlant=\"".$idpl."\";";
+    if(mysqli_query($conn, $sql)){
+        header("Location:Collection.php");
+     } else{
+         echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+     }
+     
 }
